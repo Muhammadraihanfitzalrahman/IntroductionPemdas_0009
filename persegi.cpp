12c2b8aa6cfddc0c15439973ee4e5d02c0095e7f
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-02-24 16:00:28 +0700

    first commit

---
+++ persegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ // start
+    float p, l, Luas;
+
+    // langkah 2 : Masukkan Panjang
+    cout << "Masukkan panjang : ";
+    cin >> p;
+    // langkah 3 : Masukkan Lebar
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+    // langkah 4 : Hitung Luas
+    Luas = p * l;
+    // langkah 5 : Tampilkan Luas
+    cout << "Hasilnya = " << Luas << endl;
+} // selesai
